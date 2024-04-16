86676246a87ca09dfb3f653a4350b3d7c5d9316e
cherman27 <carterherman27@gmail.com>
2024-04-16 02:56:35 -0400

    updates

--- fullstackJS/javascript/tic-tac-toe/scripts.js
+++ fullstackJS/javascript/tic-tac-toe/scripts.js
@@ -1,55 +1,91 @@
+let game = createGame()
 let gameSquares = document.getElementsByClassName("game-square");
+let newGameButton = document.getElementsByClassName("new-game-btn")[0];
 
 function gameBoard () {
-    const cells = new Array(9).fill(null);
+    let cells = new Array(9).fill(null);
 
-    const pTurn = (choice, token) => cells[choice] = token;
+    let pTurn = (choice, token) => cells[choice] = token;
 
-    const checkWin = () => {
-        for (i = 0; i < 3; i++) {
-            if (typeof cells[i] === "number") {  
-                if ((cells[i] === cells[i+1]) && (cells[i+1] === cells[i+2])) {
-                    return true;
-                }
-                if ((cells[i] === cells[i+3]) && (cells[i+3] === cells[i+6])){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    return { cells, pTurn, checkWin };
+    return { cells, pTurn };
 }
 
-function Player (token) {
-    const playerNumber = token;
-    const wins = 0;
+function checkWin(cells) {
+    const winCond = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]        
+    ];
 
+    for (i = 0; i < winCond.length; i++) {
+        let a = winCond[i][0];
+        let b = winCond[i][1];
+        let c = winCond[i][2];
+        if ((cells[a] === cells[b]) && (cells[a] === cells[c])) {
+            if (typeof cells[a] === 'number') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function Player (token, symbol) {
+    const playerNumber = token;
+    const playerSymbol = symbol;
+    let wins = 0;
     const getWins = () => wins;
     const addWin = () => wins++;
 
-    return { playerNumber, wins, getWins, addWin };
+    return { playerNumber, playerSymbol, wins, getWins, addWin };
 }
 
-function playGame() {
-    const newGame = gameBoard();
-    const playerOne = Player(0);
-    const playerTwo = Player(1);
+function createGame() {
+    let board = gameBoard();
+    let playerOne = Player(1, "X");
+    let playerTwo = Player(2, "O");
     let currPlayer = playerOne;
-    let nextPlayer = () => {
-        if (currPlayer === playerOne){
-            return playerTwo;
+    function nextPlayer() {
+        if (game.currPlayer.playerNumber == 1) {
+            game.currPlayer = game.playerTwo;
         }
-        else {
-            return playerOne;
+        else if (game.currPlayer.playerNumber == 2) {
+            game.currPlayer = game.playerOne;
         }
     }
+    return { board, playerOne, playerTwo, currPlayer, nextPlayer };
+}
 
-    while (newGame.checkWin() === false){
-        pChoice = currChoice;
+function newGame() {
+    game.board.cells = new Array(9).fill(null);
+    for (i = 0; i < gameSquares.length; i++) {
+        gameSquares[i].textContent = "";
+    }
+    updateDOM();
+}
 
-        newGame.pTurn(pChoice, currPlayer.playerNumber);
-        currPlayer = nextPlayer();
+function makeClickable(cell, idx) {
+    cell.addEventListener("click", () => {
+        if (game.board.cells[idx] === null){
+            game.board.cells[idx] = game.currPlayer.playerNumber;
+            cell.textContent = game.currPlayer.playerSymbol;
+            if (checkWin(game.board.cells)) {
+                alert(`Player ${game.currPlayer.playerNumber} won!`)
+                newGame();
+            }
+            else {
+                game.nextPlayer();
+            }
+        }
+    });
+}
+
+newGameButton.addEventListener("click", () => {
+    newGame();
+})
+
+function updateDOM() {
+    for (i = 0; i < gameSquares.length; i++){
+        makeClickable(gameSquares[i], i);
     }
 }
